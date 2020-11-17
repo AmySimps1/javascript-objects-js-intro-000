e9231ca16264ada76b2d9049ed47c7d997e56bb7
Amy Simpson <simpson.r.amy@gmail.com>
2020-11-17 20:43:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -31,13 +31,13 @@ var playlist = {taylorswift: "red"}
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-	playlist.artistName = songTitle
+	playlist[artistName] = songTitle
 	return playlist
 }
 
 
 function removeFromPlaylist(playlist, artistName){
-	delete playlist.artistName
+	delete playlist[artistName]
 	return playlist
 	
 }
