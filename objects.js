5d519cfba7e70de6a823336aa87ac2a73e6788e3
Amy Simpson <simpson.r.amy@gmail.com>
2020-11-17 20:42:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -26,3 +26,18 @@
 
 // /* Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
 // */
+
+var playlist = {taylorswift: "red"}
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+	playlist.artistName = songTitle
+	return playlist
+}
+
+
+function removeFromPlaylist(playlist, artistName){
+	delete playlist.artistName
+	return playlist
+	
+}
